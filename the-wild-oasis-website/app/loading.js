18ed4163741b3bdede677627c928f0abf1b34d28
Artur <artur.talimonchyk@gmail.com>
2024-12-22 13:14:16 +0100

    individual cabin

--- the-wild-oasis-website/app/loading.js
+++ the-wild-oasis-website/app/loading.js
@@ -1,3 +1,4 @@
+import Spinner from '@/app/_components/Spinner'
 export default function Loading(){
-    return <p>Loading data...</p>
+    return <Spinner/>
 }
--- the-wild-oasis-website/next.config.mjs
+++ the-wild-oasis-website/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'lfijklzpszkjdjzyuoco.supabase.co',
+            port: '',
+            pathname: '/storage/v1/object/public/cabin-images/**',
+            search: '',
+        },
+        ],
+  },};
 
 export default nextConfig;
